a0c0baf27b3c074dee7bb033719ab59915d1770d
jjuuaaee <seok2299@naver.com>
2023-04-07 17:57:47 +0900

    while

---
+++ study5.java
@@ -0,0 +1,17 @@
+
+public class study5 {
+	
+	public static void main(String[] args) {
+		
+		int a = 0;
+		while (a < 10) {
+		    a++;
+		    if (a % 2 == 0) {
+		        continue;
+		    }
+		    System.out.println(a);
+		}
+		
+	}
+
+}
