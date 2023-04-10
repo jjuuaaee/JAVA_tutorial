497aecbb5dacc4d9bcb2bb72a0ea8167e82c8257
jjuuaaee <seok2299@naver.com>
2023-04-10 17:36:13 +0900

    sysout

---
+++ study_base.java
@@ -0,0 +1,15 @@
+
+public class study_base {
+
+	public static void main(String[] args) {
+		
+		String busNo = "상암08";
+		int minute = 3;
+		double km = 1.2;
+		
+		System.out.println("버스번호는 "+ busNo +"번 입니다.");
+		System.out.println("남은 시간은 "+ minute + "분 입니다.");
+		System.out.println("남은 거리는 "+ km +"km 입니다.");
+		
+	}
+}
